527465ec0af8962d1a3efef3557c4f70b244cafa
tauseef989 <tauseef989@gmail.com>
2023-11-18 16:05:52 +0530

    dom1

---
+++ dom.js
@@ -0,0 +1,6 @@
+const titleElement = document.getElementById("pageTitle");
+titleElement.style.border = "2px solid black";
+
+const addItemButton = document.getElementById("addItemButton");
+addItemButton.style.fontWeight = "bold";
+addItemButton.style.color = "green";
