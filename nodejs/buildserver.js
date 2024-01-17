73e085db2ea763e45770d1ff4172aaf6400ec957
tauseef989 <tauseef989@gmail.com>
2024-01-17 10:10:14 +0530

    handlediffurl

--- nodejs/buildserver.js
+++ nodejs/buildserver.js
@@ -1,5 +1,18 @@
 const http=require("http");
 const server=http.createServer((req,res)=>{
-  console.log("tauseef ahmad");
+  console.log(req.url,req.headers,req.method);
+  res.setHeader('content-type','text/html');
+  if (req.url === "/home") {
+    res.write("<html><body><h1>Welcome home</h1></body></html>");
+} else if (req.url === "/about") {
+    res.write("<html><body><h1>Welcome to About Us page</h1></body></html>");
+} else if (req.url === "/node") {
+    res.write("<html><body><h1>Welcome to my Node.js project</h1></body></html>");
+} else {
+    // Default response for other URLs
+    res.write("<html><body><h1>Page not found</h1></body></html>");
+}
+  res.end();
+
 });
 server.listen(4000);
