20fba0193ef421a912fbc107c3670f3e552673b4
tauseef989 <tauseef989@gmail.com>
2024-01-18 14:04:25 +0530

    reqandres

--- nodejs/first-app.js
+++ nodejs/first-app.js
@@ -1,1 +1,40 @@
-console.log('hello from node.js');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  const method = req.method;
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>");
+    const messageContent = fs.readFileSync("message.txt", "utf8");
+    if (messageContent) {
+      res.write("<p>Message from file: " + messageContent + "</p>");
+    }
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFileSync("message.txt", message);
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+});
+
+server.listen(4000);
