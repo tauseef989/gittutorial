b7723ff5f64de5262de8f578ec613aa758a5c9bd
tauseef989 <tauseef989@gmail.com>
2024-01-19 12:12:58 +0530

    cleanupcode

---
+++ nodejs/exp.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const modulehandler=(req,res)=>{
+  const method = req.method;
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write("<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>");
+    const messageContent = fs.readFileSync("message.txt", "utf8");
+    if (messageContent) {
+      res.write("<p>Message from file: " + messageContent + "</p>");
+    }
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+
+      fs.writeFileSync("message.txt", message);
+
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  }
+}
+module.exports=modulehandler;
+// module.export.modulehandler
+// module.export={
+//   handler:modulehandler
+//   text:from other file
+// }
