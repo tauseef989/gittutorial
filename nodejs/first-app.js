844cbefaf0d49bf8970f5770b5eff82f3f22b799
tauseef989 <tauseef989@gmail.com>
2024-01-20 16:04:30 +0530

    expressjsbodyparser

--- nodejs/first-app.js
+++ nodejs/first-app.js
@@ -1,16 +1,21 @@
-const http = require('http');
-const express =require("express");
-const app=express();
-app.use((req,res,next)=>{
-  console.log("qwerty")
-  next()
+const express = require("express");
+const bodyparser=require("body-parser");
+const app = express();
+app.use(bodyparser.urlencoded({extended:false}));
 
+app.use("/add-product", (req, res, next) => {
+  console.log("qwerty");
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">submit</button></form>');
 });
-app.use((req,res,next)=>{
-  console.log("wasd")
-  res.send("<h1>hi tauseef here</h1>")
-  
+app.post("/product",(req,res,next)=>{
+  console.log(req.body)
 
+  res.redirect("/");
+
+})
+app.use("/", (req, res, next) => {
+  console.log("wasd");
+  res.send("<h1>hi</h1>");
 });
 
 app.listen(4000);
