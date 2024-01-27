432cf675e355a8042face702f8c59c9c356f2b64
tauseef989 <tauseef989@gmail.com>
2024-01-27 13:35:02 +0530

    abed

--- nodejs/first-app.js
+++ nodejs/first-app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const path=require("path");
 const bodyparser=require('body-parser');
 const app = express();
+const rootDir = require('./util/path');
 const routesadmin=require('./routes/admin');
 const routesshop=require('./routes/shop');
 app.use(bodyparser.urlencoded({extended:false}));
 app.use('/admin',routesadmin);
 app.use(routesshop);
 app.use((req,res,next)=>{
-  res.status(404).send('<h1>page not found</h1>')
+  res.status(404).sendFile(path.join(rootDir,'views','404.html'));
 })
 app.listen(4000, () => {
   console.log('Server is running on port 4000');
