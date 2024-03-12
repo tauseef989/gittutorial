1778d8eecfb8144e0be1e8c4b646bfedb077c747
tauseef989 <tauseef989@gmail.com>
2024-03-12 12:24:29 +0530

    youarepremiumuser

--- nodejs/expensesignup/app.js
+++ nodejs/expensesignup/app.js
@@ -8,14 +8,15 @@ const path = require('path');
 const jwt=require("jsonwebtoken")
 const crypto=require("crypto")
 const Razorpay=require('razorpay')
-const secretKey = crypto.randomBytes(32).toString('hex');
+const secretKey ='e314d73d2ee88c916172ee2b4a82b4a44f0c70db5bfe8c303a30607b8b59a462'
 const PORT = process.env.PORT || 8000;
 const razorpayKeyId = process.env.RAZORPAY_KEY_ID;
 const razorpayKeySecret = process.env.RAZORPAY_KEY_SECRET;
-console.log(razorpayKeyId,razorpayKeySecret)
 
-
-
+const orderrouter=require("./router/orders")
+const expensesrouter=require("./router/expenses")
+const signuprouter=require("./router/signup")
+const loginrouter=require('./router/login')
 
 
 const app = express();
@@ -33,123 +34,23 @@ app.use(bodyParser.json());
 function generateToken(id){
   return jwt.sign({userid:id},secretKey)
 }
+app.use('/purchase',orderrouter)
+app.use('/expenses', expensesrouter)
+app.use(signuprouter)
+app.use(loginrouter)
 
-app.post("/purchase/updatetransactionstatus", async (req, res) => {
-  console.log("apple")
+app.get('/ispremiummember', async (req, res) => {
   try {
-    console.log("apple")
-    const { order_id, payment_id } = req.body;
-    
-    // Get a connection from the pool
-    const connection = await pool.getConnection();
-    // Begin the transaction
-    await connection.beginTransaction();
-    try {  
-      // Update the transaction status in the database
-      await connection.execute("UPDATE orders SET status = 'COMPLETED' WHERE order_id = ?", [order_id]);
-      await connection.execute("UPDATE orders SET payment_id = ? WHERE order_id = ?", [payment_id, order_id]);
+    const token = req.header('Authorization');
+    const userid = jwt.verify(token, secretKey);
 
-      // Commit the transaction
-      await connection.commit();
+    const [rows] = await pool.execute('SELECT user_id, payment_id FROM orders WHERE user_id = ? AND payment_id IS NOT NULL', [userid.userid]);
 
-      // Release the connection back to the pool
-      connection.release();
-
-      // Return a success message
-      res.json({ message: "Transaction status updated successfully" });
-    } catch (error) {
-      // Rollback the transaction in case of an error
-      await connection.rollback();
-      console.error("Error updating transaction status:", error);
-      res.status(500).json({ error: "Failed to update transaction status" });
-    } finally {
-      // Ensure to release the connection in case of any error
-      if (connection) {
-        connection.release();
-      }
+    if (rows.length !== 0) {
+      res.status(200).json({ key: true });
+    } else {
+      res.status(200).json({ key: false });
     }
-  } catch (error) {
-    console.error("Error establishing database connection:", error);
-    res.status(500).json({ error: "Database connection error" });
-  }
-});
-
-
-// app.get("/purchase/premiummemebership", async (req,res)=>{
-//   try{
-//     var rzp=new Razorpay({
-//       key_id:process.env.RAZORPAY_KEY_ID,
-//       key_secret:process.env.RAZORPAY_KEY_SECRET
-//     });
-//     const amount=2500;
-//     rzp.orders.create({amount,currency:'INR'},(err,order)=>{
-//       if(err){
-//         throw new Error(JSON.stringify(err));
-//       }
-//       req.user.createOrder({orderid:order.id, status:'PENDING'}).then(()=>{
-//         return res.status(201).json({order,key_id:rzp.key_id});
-//       }).catch(err=>{
-//         throw new Error(err);
-//       });
-//     });
-//   } catch (err) {
-//     console.log(err);
-//     res.status(403).json({message:'something went wrong',error:err});
-//   }
-// });
-app.get("/purchase/premiummembership", async (req, res) => {
-  const token=req.header('Authorization')
-  // console.log('premiummembership',token)
-  const userid=jwt.verify(token,secretKey)
-  // console.log(userid)
-  try {
-    const rzp = new Razorpay({
-      key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_KEY_SECRET
-    });
-
-    const amount = 2500;
-
-    // Create a new order using async/await
-    const order = await new Promise((resolve, reject) => {
-      rzp.orders.create({ amount, currency: 'INR' }, (err, order) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(order);
-        }
-      });
-    });
-
-    await pool.execute('INSERT INTO orders(order_id, status, user_id) VALUES (?, ?, ?)', [order.id, 'PENDING', userid.userid]);
-
-    // Return the response
-    console.log("getsucessfull-123")
-    res.status(201).json({ order, key_id: rzp.key_id });
-  } catch (err) {
-    console.error(err);
-    res.status(403).json({ message: 'Something went wrong', error: err });
-  }
-});
-
-
-
-app.post("/login", async (req, res) => {
-  const { Email, Password } = req.body;
-  try {
-    const [rows] = await pool.execute("SELECT * FROM users WHERE email=?", [Email]);
-    if (rows.length === 0) {
-      return res.status(401).json({ error: "Invalid email or password" });
-    }
-    const user = rows[0];
-    const isValidPassword = await bcrypt.compare(Password, user.password);
-    if (!isValidPassword) {
-      return res.status(401).json({ error: "Invalid email or password" });
-    }
-    console.log("Login successful:", user); // Logging the user data retrieved from the database
-    // res.redirect('/expenses/expense'); // Redirect to expense.html upon successful login
-    res.send({ message: 'Login successfully', token: generateToken(user.id) });
-
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "An internal server error occurred" });
@@ -157,69 +58,174 @@ app.post("/login", async (req, res) => {
 });
 
 
-app.post("/signup", async (req, res) => {
-  const { Name, Email, Password } = req.body;
-  try {
-    const hashedpassword= await bcrypt.hash(Password,10)
-    await pool.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [Name, Email, hashedpassword]);
-    console.log(Name, Email, hashedpassword);
-    res.send("Received successfully");
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).send("Error occurred");
-  }
-});
-
-
-// Endpoint to handle expense creation
-app.post('/expenses', async (req, res) => {
-  const token=req.header('Authorization')
-  console.log('alpha',token)
-  const userid=jwt.verify(token,secretKey)
-  const { amount, description, category } = req.body;
-
-  try {
-    // Insert expense into the database
-    await pool.execute('INSERT INTO expenses (amount, description, category,userid) VALUES (?, ?, ?, ?)', [amount, description, category,userid.userid]);
-    res.status(201).json({ message: 'Expense added successfully' });
-  } catch (error) {
-    console.error('Error adding expense:', error);
-    res.status(500).json({ error: 'Failed to add expense' });
-  }
-});
-
-// Endpoint to fetch all expense
-app.get('/expenses', async (req, res) => {
-  try {
-    const token=req.header('Authorization')
-    console.log('beta',token)
-    const userid=jwt.verify(token,secretKey)
-    console.log(userid)
-    // Fetch all expenses from the database
-    const [expenses] = await pool.execute('SELECT * FROM expenses WHERE userid=?',[userid.userid]);
-    console.log(expenses)
-    res.json(expenses);
-  } catch (error) {
-    console.error('Error fetching expenses:', error);
-    res.status(500).json({ error: 'Failed to fetch expenses' });
-  }
-});
-
-// Endpoint to delete an expense by ID
-app.delete('/expenses/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    // Delete expense from the database
-    await pool.execute('DELETE FROM expenses WHERE id = ?', [id]);
-    res.json({ message: 'Expense deleted successfully' });
-  } catch (error) {
-    console.error('Error deleting expense:', error);
-    res.status(500).json({ error: 'Failed to delete expense' });
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+
+
+
+// app.post("/purchase/updatetransactionstatus", async (req, res) => {
+//   console.log("apple")
+//   try {
+//     console.log("apple")
+//     const { order_id, payment_id } = req.body;
+    
+//     // Get a connection from the pool
+//     const connection = await pool.getConnection();
+//     // Begin the transaction
+//     await connection.beginTransaction();
+//     try {  
+//       // Update the transaction status in the database
+//       await connection.execute("UPDATE orders SET status = 'COMPLETED' WHERE order_id = ?", [order_id]);
+//       await connection.execute("UPDATE orders SET payment_id = ? WHERE order_id = ?", [payment_id, order_id]);
+
+//       // Commit the transaction
+//       await connection.commit();
+
+//       // Release the connection back to the pool
+//       connection.release();
+
+//       // Return a success message
+//       res.json({ message: "Transaction status updated successfully" });
+//     } catch (error) {
+//       // Rollback the transaction in case of an error
+//       await connection.rollback();
+//       console.error("Error updating transaction status:", error);
+//       res.status(500).json({ error: "Failed to update transaction status" });
+//     } finally {
+//       // Ensure to release the connection in case of any error
+//       if (connection) {
+//         connection.release();
+//       }
+//     }
+//   } catch (error) {
+//     console.error("Error establishing database connection:", error);
+//     res.status(500).json({ error: "Database connection error" });
+//   }
+// });
+
+
+// app.get("/purchase/premiummembership", async (req, res) => {
+//   const token=req.header('Authorization')
+//   // console.log('premiummembership',token)
+//   const userid=jwt.verify(token,secretKey)
+//   // console.log(userid)
+//   try {
+//     const rzp = new Razorpay({
+//       key_id: process.env.RAZORPAY_KEY_ID,
+//       key_secret: process.env.RAZORPAY_KEY_SECRET
+//     });
+
+//     const amount = 2500;
+
+//     // Create a new order using async/await
+//     const order = await new Promise((resolve, reject) => {
+//       rzp.orders.create({ amount, currency: 'INR' }, (err, order) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(order);
+//         }
+//       });
+//     });
+
+//     await pool.execute('INSERT INTO orders(order_id, status, user_id) VALUES (?, ?, ?)', [order.id, 'PENDING', userid.userid]);
+
+//     // Return the response
+//     console.log("getsucessfull-123")
+//     res.status(201).json({ order, key_id: rzp.key_id });
+//   } catch (err) {
+//     console.error(err);
+//     res.status(403).json({ message: 'Something went wrong', error: err });
+//   }
+// });
+
+
+
+// app.post("/login", async (req, res) => {
+//   const { Email, Password } = req.body;
+//   try {
+//     const [rows] = await pool.execute("SELECT * FROM users WHERE email=?", [Email]);
+//     if (rows.length === 0) {
+//       return res.status(401).json({ error: "Invalid email or password" });
+//     }
+//     const user = rows[0];
+//     const isValidPassword = await bcrypt.compare(Password, user.password);
+//     if (!isValidPassword) {
+//       return res.status(401).json({ error: "Invalid email or password" });
+//     }
+//     console.log("Login successful:", user); // Logging the user data retrieved from the database
+//     // res.redirect('/expenses/expense'); // Redirect to expense.html upon successful login
+//     res.send({ message: 'Login successfully', token: generateToken(user.id) });
+
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).json({ error: "An internal server error occurred" });
+//   }
+// });
+
+
+// app.post("/signup", async (req, res) => {
+//   const { Name, Email, Password } = req.body;
+//   try {
+//     const hashedpassword= await bcrypt.hash(Password,10)
+//     await pool.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [Name, Email, hashedpassword]);
+//     console.log(Name, Email, hashedpassword);
+//     res.send("Received successfully");
+//   } catch (error) {
+//     console.error("Error:", error);
+//     res.status(500).send("Error occurred");
+//   }
+// });
+
+
+// Endpoint to handle expense creation
+// app.post('/expenses', async (req, res) => {
+//   const token=req.header('Authorization')
+//   console.log('alpha',token)
+//   const userid=jwt.verify(token,secretKey)
+//   const { amount, description, category } = req.body;
+
+//   try {
+//     // Insert expense into the database
+//     await pool.execute('INSERT INTO expenses (amount, description, category,userid) VALUES (?, ?, ?, ?)', [amount, description, category,userid.userid]);
+//     res.status(201).json({ message: 'Expense added successfully' });
+//   } catch (error) {
+//     console.error('Error adding expense:', error);
+//     res.status(500).json({ error: 'Failed to add expense' });
+//   }
+// });
+
+
+// Endpoint to fetch all expense
+// app.get('/expenses', async (req, res) => {
+//   try {
+//     const token=req.header('Authorization')
+//     console.log('beta',token)
+//     const userid=jwt.verify(token,secretKey)
+//     console.log(userid)
+//     // Fetch all expenses from the database
+//     const [expenses] = await pool.execute('SELECT * FROM expenses WHERE userid=?',[userid.userid]);
+//     console.log(expenses)
+//     res.json(expenses);
+//   } catch (error) {
+//     console.error('Error fetching expenses:', error);
+//     res.status(500).json({ error: 'Failed to fetch expenses' });
+//   }
+// });
+
+
+// Endpoint to delete an expense by ID
+// app.delete('/expenses/:id', async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     // Delete expense from the database
+//     await pool.execute('DELETE FROM expenses WHERE id = ?', [id]);
+//     res.json({ message: 'Expense deleted successfully' });
+//   } catch (error) {
+//     console.error('Error deleting expense:', error);
+//     res.status(500).json({ error: 'Failed to delete expense' });
+//   }
+// });
---
+++ nodejs/expensesignup/router/expenses.js
@@ -0,0 +1,83 @@
+const express = require('express');// Import the Express module
+const app = express();// Create an instance of the Express application
+const router = express.Router(); // Import the Router module from Express
+
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+const bcrypt=require("bcrypt")
+const path = require('path');
+const jwt=require("jsonwebtoken")
+const crypto=require("crypto")
+const Razorpay=require('razorpay')
+const secretKey ='e314d73d2ee88c916172ee2b4a82b4a44f0c70db5bfe8c303a30607b8b59a462'
+require('dotenv').config();
+
+
+
+
+const pool = mysql.createPool({
+  user: 'root',
+  database: 'expense',
+  password: 'aA@11111',
+  host: 'localhost'
+});
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+function generateToken(id){
+  return jwt.sign({userid:id},secretKey)
+}
+
+// Endpoint to handle expense creation
+router.post('/', async (req, res) => {
+  const token=req.header('Authorization')
+  console.log('alpha',token)
+  const userid=jwt.verify(token,secretKey)
+  const { amount, description, category } = req.body;
+
+  try {
+    // Insert expense into the database
+    await pool.execute('INSERT INTO expenses (amount, description, category,userid) VALUES (?, ?, ?, ?)', [amount, description, category,userid.userid]);
+    res.status(201).json({ message: 'Expense added successfully' });
+  } catch (error) {
+    console.error('Error adding expense:', error);
+    res.status(500).json({ error: 'Failed to add expense' });
+  }
+});
+
+// Endpoint to fetch all expense
+router.get('/', async (req, res) => {
+  try {
+    const token=req.header('Authorization')
+    console.log('beta',token)
+    const userid=jwt.verify(token,secretKey)
+    console.log(userid)
+    // Fetch all expenses from the database
+    const [expenses] = await pool.execute('SELECT * FROM expenses WHERE userid=?',[userid.userid]);
+    console.log(expenses)
+    res.json(expenses);
+  } catch (error) {
+    console.error('Error fetching expenses:', error);
+    res.status(500).json({ error: 'Failed to fetch expenses' });
+  }
+});
+
+// Endpoint to delete an expense by ID
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    // Delete expense from the database
+    await pool.execute('DELETE FROM expenses WHERE id = ?', [id]);
+    res.json({ message: 'Expense deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting expense:', error);
+    res.status(500).json({ error: 'Failed to delete expense' });
+  }
+});
+
+module.exports=router
