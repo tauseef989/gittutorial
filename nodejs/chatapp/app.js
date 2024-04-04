a6fcd7310f8bf8aa435f10635ce5dc91ab376afb
tauseef989 <tauseef989@gmail.com>
2024-04-04 13:32:43 +0530

    integratingfb

--- nodejs/chatapp/app.js
+++ nodejs/chatapp/app.js
@@ -1,4 +1,3 @@
-const express=require('express')
 require('dotenv').config();
 const express = require("express");
 const fs=require('fs')
@@ -21,12 +20,22 @@ app.use(bodyParser.json());
 app.post('/signup',async (req, res) => {
   const { Name, Email, Number,Password } = req.body;
   try {
-    const hashedpassword= await bcrypt.hash(Password,10)
-    await pool.execute("INSERT INTO users (name, email,number, password) VALUES (?, ?, ?,?)", [Name, Email,Number, hashedpassword]);
-    console.log(Name, Email, hashedpassword);
-    res.send("Received successfully");
+    const [rows] = await pool.execute('SELECT * FROM users WHERE email = ?', [email]);
+    if (rows.length > 0) {
+      res.send("User already exists, Please Login")
+   
+    } else {
+      const hashedpassword= await bcrypt.hash(Password,10)
+      await pool.execute("INSERT INTO users (name, email,number, password) VALUES (?, ?, ?,?)", [Name, Email,Number, hashedpassword]);
+      console.log(Name, Email, hashedpassword);
+      res.send("Received successfully");
+    
+    }
+
   } catch (error) {
     console.error("Error:", error);
     res.status(500).send("Error occurred");
   }
-};)
+});
+
+app.listen(8000)
